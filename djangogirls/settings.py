9e8e44c94348dd84aed849d4eb91a5512d415e05
5kyler <solideogloria1130@gmail.com>
2019-11-11 19:37:10 +0900

    extends template

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+# BASE_DIR = djangogirls directory
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
